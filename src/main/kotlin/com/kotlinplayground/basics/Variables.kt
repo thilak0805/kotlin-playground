089cf8dbc243c65cb6eee18ed618be6dc48dfdc8
thilak0805 <daya.22.thilak@gmail.com>
2025-10-15 17:05:41 +0530

    variables

---
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,17 @@
+package org.modules.com.kotlinplayground.basics
+
+fun main(){
+    //immutable
+    val name : String = "Daya"
+
+    println(name)
+
+    //mutable
+    var age : Int =34
+
+    println(age)
+
+    age = 35
+    println(age)
+
+}
