06172e714dec094f774079ef8011bb1c28d3ddc4
thilak0805 <daya.22.thilak@gmail.com>
2025-10-18 21:38:06 +0530

    while and do while

--- src/main/kotlin/com/kotlinplayground/basics/LoopsAndRanges.kt
+++ src/main/kotlin/com/kotlinplayground/basics/LoopsAndRanges.kt
@@ -1,7 +1,7 @@
 package org.modules.com.kotlinplayground.basics
 
 fun main(){
-    val range = 1..10;
+   /* val range = 1..10;
 
     for(i in range){
         println("i : $i")
@@ -17,7 +17,29 @@ fun main(){
     //skip values
     for(i in reverseRange step 2){
         println("reverseRange with step : $i")
-    }
+    }*/
 
+    //while and do while
+
+    exploreWhile()
+    exploreDoWhile()
+
+}
+
+fun exploreDoWhile() {
+   var i = 0
+    do {
+        println("i value is inside dowhile loop: $i")
+        i++
+    }while(i<5)
+}
+
+fun exploreWhile(){
+
+    var x = 1
+    while(x < 5) {
+        println("value of x is :$x")
+        x++
+    }
 
 }
