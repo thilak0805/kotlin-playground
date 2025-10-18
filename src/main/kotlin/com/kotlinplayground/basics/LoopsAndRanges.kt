dd52b990a34a624dbc7c03dd11c5ed4a055b323c
thilak0805 <daya.22.thilak@gmail.com>
2025-10-18 21:08:33 +0530

    Ranges and for loops

---
+++ src/main/kotlin/com/kotlinplayground/basics/LoopsAndRanges.kt
@@ -0,0 +1,23 @@
+package org.modules.com.kotlinplayground.basics
+
+fun main(){
+    val range = 1..10;
+
+    for(i in range){
+        println("i : $i")
+    }
+
+    //reverse range using downTo
+
+    val reverseRange = 10 downTo 1
+    for(i in reverseRange){
+        println("reverseRange : $i")
+    }
+
+    //skip values
+    for(i in reverseRange step 2){
+        println("reverseRange with step : $i")
+    }
+
+
+}
