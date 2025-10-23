e5aa2de3443e31c42dcb9077d942fffb4ec8f313
thilak0805 <daya.22.thilak@gmail.com>
2025-10-23 14:11:35 +0530

    default and named parameter functions

--- src/main/kotlin/com/kotlinplayground/functions/functions.kt
+++ src/main/kotlin/com/kotlinplayground/functions/functions.kt
@@ -1,5 +1,7 @@
 package org.modules.com.kotlinplayground.functions
 
+import java.time.LocalDate
+
 fun main(){
    val unit =  printname("daya")
     println("Unit is : $unit")
@@ -7,6 +9,16 @@ fun main(){
     println("Result is $result")
     val result1 = addition_approach1(1, 2)
     println("Result1 is $result1")
+
+    //default parameter
+    printPersonDetails("Daya","abc@email.com", LocalDate.parse("1983-05-08"))
+    printPersonDetails("Daya1")
+
+    //named parameters
+    printPersonDetails(name="Daya2",dob=LocalDate.parse("2025-08-01"))
+    printPersonDetails(dob=LocalDate.parse("2025-08-01"), name="Daya3",email="abc@gmail.com")
+
+
 }
 
 // after paranthesis : Int is the return type in function declaration
@@ -29,4 +41,14 @@ fun addition_approach2(x: Int, y: Int) : Int  = x+y
 // if any function that does not return anything then we can use Unit as return type
 fun printname(name: String): Unit{
     println("name is :$name")
+}
+
+//default parameters
+fun printPersonDetails(
+    name : String,
+    //if the email and dob is not passed, a default value is assigned
+    email :String = "",
+    dob: LocalDate = LocalDate.now()
+){
+    println("Name is $name and email is $email and dob is $dob")
 }
