ba72b28c3857c279bf3a3b63c740ea0777c0e856
thilak0805 <daya.22.thilak@gmail.com>
2025-10-27 15:39:06 +0530

    init block

--- src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -13,12 +13,20 @@ class Person(val name : String = "",
     // while printing, since we are able to print only email, we need to print name,age. inorder to initialize the default constructor
     // person, we are using 'this' keyword to initialize name and age
     var email : String = ""
+
+    // init block can be used to run some initialization logic during the instance creation
+    var nameLength : Int = 0
+    init {
+        println(" Inside init ...")
+        nameLength = name.length
+    }
     constructor(_email : String,
         _name : String ="",
         _age : Int = 0) :this(_name, _age) {
         email = _email
 
     }
+
     fun action(){
         println("Person Walks")
     }
@@ -27,19 +35,24 @@ class Person(val name : String = "",
 fun main(){
     // creating the instance of person class, no need to use new keyword to create an instance of the class
     //in java we normally use getters and setters to access the properties, but in kotlin you can access the properties as fields
-    val person = Person("Daya", 26)
+
+
+  /*  val person = Person("Daya", 26)
     person.action()
-    println("Name : ${person.name} and the Age is : ${person.age}")
+    println("Name : ${person.name} and the Age is : ${person.age}")*/
 
     // if you want to create the instance of the person class without passing the name and age, it is possible by using the default values concept
     // in the person constructor declaration, we can add that, if the name is passed we take values otherwise we provide default values for that parameter
     // its a kind of overloaded constructor, if we pass value it will take it, otherwise it will take default value
-    val person1 = Person()
-    println("Default Name : ${person1.name} and the Default Age is : ${person1.age}")
+
+   /* val person1 = Person()
+    println("Default Name : ${person1.name} and the Default Age is : ${person1.age}")*/
 
     // here we are using named parameter concept to invoke the constructor which is having email parameter
+
+
     val person2 = Person(_email = "abc@gmail.com","Alex",25)
     println("person2  Name : ${person2.name}  and the person2 Age is : ${person2.age}" +
-            "and the person2 email is :${person2.email}")
+            "and the person2 email is :${person2.email} and the nameLength is :${person2.nameLength}")
 
 }
