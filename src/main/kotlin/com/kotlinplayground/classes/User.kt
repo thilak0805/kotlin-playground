45c60616f2954ded41272dcc4dc04496937cf9f9
thilak0805 <daya.22.thilak@gmail.com>
2025-10-29 22:43:46 +0530

    Inheritance - overriding variables, open and override keywords

--- src/main/kotlin/com/kotlinplayground/classes/User.kt
+++ src/main/kotlin/com/kotlinplayground/classes/User.kt
@@ -1,8 +1,11 @@
 package org.modules.com.kotlinplayground.classes
 
 // if we remove open identifier, this class cannot be extendable, it cannot be subclassed
+
 open class User(val name : String) {
-    fun login(){
+    open var isLoggedIn : Boolean = false
+    // if you want this fun login to be override by subclass, then it should be declared as open, because by default it is final
+   open fun login(){
         println("inside user login")
     }
 }
@@ -14,7 +17,16 @@ open class User(val name : String) {
 
 //open class abc{}
 // we cannot extend Student class like this class Student(name : String) : User(name), abc, it will compilation issue
-class Student(name : String) : User(name)
+class Student(name : String) : User(name) {
+    // in this student subclass, kotlin allows to override the function login using override keyword, make sure the function name
+    // and method signature are same
+   override var isLoggedIn : Boolean = false
+    override fun login(){
+        println("inside student login")
+        // if you need to invoke super class login method, use super keyword here
+        super.login()
+    }
+}
 
 class Instructor(name : String) : User(name)
 
@@ -22,6 +34,8 @@ fun main(){
     val student = Student("Alex")
     println("Student name is : ${student.name}")
     student.login();
+    student.isLoggedIn = true
+    println("isLogged in value  : ${student.isLoggedIn}")
 
     val instructor = Instructor("Philip")
     println("instructor name is : ${instructor.name}")
